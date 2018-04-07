609b3d3d21d9aeb495097e3ceae4c3ad5855a0fb
Martina Toshevska <martina.tosevska.95@gmail.com>
2018-04-07 02:54:36 +0200

    Finish code for Supervised Random Walk implementation

--- supervised_random_walks.py
+++ supervised_random_walks.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import fmin_l_bfgs_b
+import functools
 
 
 def logistic_function(x, b):
@@ -7,6 +8,13 @@ def logistic_function(x, b):
     Logistic function - Wilcoxon-Mann-Whitney (WMW) loss
     """
     return 1.0 / (1 + np.exp(-1 * x / b))
+
+
+def derivative_logistic_function(x, b):
+    """
+    Derivative of logistic function
+    """
+    return (logistic_function(x, b) * (1 - logistic_function(x, b)))/b
 
 
 def get_differences(p, l_set, d_set):
@@ -37,6 +45,19 @@ def logistic_edge_strength_function(w, features):
     :rtype: numpy.array
     """
     return logistic_function(features.dot(w), 1)
+
+
+def logistic_edge_strength_derivative_function(w, features):
+    """ Derivative of logistic edge strength function
+
+    :param w: weight vector
+    :type w: numpy.array
+    :param features: feature vector
+    :type features: numpy.array
+    :return: edge strengths
+    :rtype: numpy.array
+    """
+    return np.multiply(derivative_logistic_function(np.transpose(np.dot(features, w)), 1), np.transpose(features))
 
 
 def get_stochastic_transition_matrix(A):
@@ -82,6 +103,53 @@ def loss_function(diff, b):
     return logistic_function(diff, b)
 
 
+def gradient_function(graph, features, sources, destinations, alpha, max_iter,
+                      lambda_param, epsilon, small_epsilon, margin_loss, w):
+    """ Gradient function
+
+    :param graph: igraph object
+    :type graph: igraph.Graph
+    :param features: feature vector
+    :type features: numpy.array
+    :param w: parameter vector
+    :type w: numpy.array
+    :param sources: list of indices of source nodes
+    :type sources: list(int)
+    :param destinations: list of indices of destination nodes
+    :type destinations: list(int)
+    :param alpha: restart probability
+    :type alpha: float
+    :param max_iter: maximum number of iterations
+    :type max_iter: int
+    :param lambda_param: regularization parameter
+    :type lambda_param: float
+    :param epsilon: tolerance parameter for page rank convergence
+    :type epsilon: float
+    :param small_epsilon: change parameter in strengths of the edges
+    :rtype small_epsilon: float
+    :type small_epsilon: float
+    :param margin_loss: margin loss
+    :type margin_loss: float
+    :return: values of the gradient function
+    :rtype: numpy.array
+    """
+    gr = np.zeros(len(w))
+    strengths = logistic_edge_strength_function(w, features) + small_epsilon
+    graph.es['strength'] = strengths.flatten()
+    A = np.array(graph.get_adjacency(attribute='strength').data)
+    Q_prim = get_stochastic_transition_matrix(A)
+    for source, i in zip(sources, range(len(sources))):
+        Q = get_transition_matrix(Q_prim, source, alpha)
+        p = iterative_page_rank(Q, epsilon, max_iter)
+        dp = iterative_page_rank_derivative(p, Q, epsilon, max_iter, w, features, strengths, graph, Q, alpha)
+        l_set = list(set(graph.vs.indices) - set(destinations[i] + [source]))
+        diff = get_differences(p, l_set, destinations)
+        dh = derivative_logistic_function(diff, margin_loss)
+        for k in range(len(w)):
+            gr[k] += 2 * w[k] + lambda_param * np.sum(dh * get_differences(dp[:, k], l_set, destinations))
+    return gr
+
+
 def iterative_page_rank(trans, epsilon, max_iter):
     """ Iterative power-iterator like computation of PageRank vector p
 
@@ -94,22 +162,71 @@ def iterative_page_rank(trans, epsilon, max_iter):
     :return: stationary distribution
     :rtype: numpy.array
     """
-    p = np.sum(trans > 0, axis=1)
+    p = np.ones((1, trans.shape[0])) / trans.shape[0]
     p_new = np.dot(p, trans)
     while not (np.allclose(p, p_new, rtol=0, atol=epsilon) or max_iter <= 0):
         p = p_new
         p_new = np.dot(p, trans)
         max_iter -= 1
-    return p_new
+    return p_new[0]
 
 
-def objective_function(graph, features, w, sources, destinations, alpha, max_iter,
-                       lambda_param, epsilon, small_epsilon, margin_loss):
+def iterative_page_rank_derivative(p, trans, epsilon, max_iter, w, features, strengths, graph, Q, alpha):
+    """ Derivative of PageRank vector p
+
+    :param p: PageRank vector
+    :param p: numpy.array
+    :param trans: transition matrix
+    :type trans: numpy.array
+    :param epsilon: tolerance parameter
+    :type epsilon: float
+    :param max_iter: maximum number of iterations
+    :type max_iter: int
+    :param w: parameter vector
+    :type w: numpy.array
+    :param features: feature vector
+    :type features: numpy.array
+    :param strengths: edge strengths
+    :type strengths: numpy.array
+    :param graph: igraph object
+    :type graph: igraph.Graph
+    :param Q: transition matrix
+    :type Q: numpy.array
+    :param alpha: restart probability
+    :type alpha: float
+    :return: derivative of PageRank vector
+    :rtype: numpy.array
+    """
+    dp = np.zeros((trans.shape[0], len(w)))
+    dstrengths = logistic_edge_strength_derivative_function(w, features)
+    for k in range(len(w)):
+        t = 0
+        graph.es['strength'] = strengths.flatten()
+        graph.es['temp'] = np.transpose(dstrengths)[:, k].flatten()
+        A = np.array(graph.get_adjacency(attribute='strength').data)
+        dA = np.array(graph.get_adjacency(attribute='temp').data)
+        A_rowsum = np.diag(A.sum(axis=1))
+        dA_rowsum = np.diag(dA.sum(axis=1))
+        rec = np.linalg.matrix_power(A_rowsum, -2)
+        dQk = (1 - alpha) * np.dot(rec, np.dot(A_rowsum, dA) - np.dot(dA_rowsum, A))
+        while True:
+            t += 1
+            dp_new = np.dot(dp[:, k], Q) + np.dot(p, dQk)
+            pre = np.copy(dp[:, k])
+            dp[:, k] = dp_new
+            if max(abs(pre - dp[:, k])) < epsilon or t > max_iter:
+                break
+    return dp
+
+
+def objective_function(graph, features, sources, destinations, alpha, max_iter,
+                       lambda_param, epsilon, small_epsilon, margin_loss, w):
     """ Objective function for minimization in the training process
 
     :param graph: igraph object
     :type graph: igraph.Graph
-    :param features:
+    :param features: feature vector
+    :type features: numpy.array
     :param w: parameter vector
     :type w: numpy.array
     :param sources: list of indices of source nodes
@@ -174,9 +291,18 @@ def supervised_random_walks(graph, sources, destinations, alpha=0.3, lambda_par=
 
     w = np.ones((len(graph.es.attributes()), 1))
 
-    objective_function(graph, features, w, sources, destinations, alpha,
-                       max_iter, lambda_par, epsilon, small_epsilon, margin_loss)
+    '''    
+    gradient_function(graph, features, sources, destinations, alpha, 
+                      max_iter, lambda_par, epsilon, small_epsilon, margin_loss, w)
 
-    w_optimized = ...
+    objective_function(graph, features, sources, destinations, alpha,
+                       max_iter, lambda_par, epsilon, small_epsilon, margin_loss, w)
+    '''
+
+    w_optimized = fmin_l_bfgs_b(functools.partial(objective_function, graph, features, sources, destinations, alpha,
+                                                  max_iter, lambda_par, epsilon, small_epsilon, margin_loss),
+                                w,
+                                functools.partial(gradient_function, graph, features, sources, destinations, alpha,
+                                                  max_iter, lambda_par, epsilon, small_epsilon, margin_loss))
 
     return w_optimized
--- train.py
+++ train.py
@@ -1,17 +1,29 @@
+import numpy as np
 from igraph import *
 import pandas as pd
-import numpy as np
 from supervised_random_walks import supervised_random_walks
+import time
 
 
 graph_data = pd.read_csv('toy_graph.csv', header=0)
-graph = Graph()
+graph = Graph(directed=True)
 vertices = np.unique(np.concatenate((graph_data[['from']].values, graph_data[['to']].values))).tolist()
 graph.add_vertices(vertices)
 graph.add_edges(graph_data[['from', 'to']].values)
+'''
 for feature in graph_data.columns[2:]:
-    graph.es[feature] = graph_data[feature].values.tolist()
+    m = np.mean(graph_data[feature].values)
+    stddev = np.std(graph_data[feature].values)
+    graph.es[feature] = ((graph_data[feature].values - m) / stddev).tolist()
+'''
+
+graph.es['prop1'] = [-0.8400269, 0.5040161, -0.8400269, 0.5040161, 1.8480591, -0.8400269, 0.5040161, -0.8400269]
+graph.es['prop2'] = [-0.3535534, 1.0606602, 1.0606602, -0.3535534, -0.3535534, 1.0606602, -1.7677670, -0.3535534]
+
+graph.es['const'] = np.ones(graph_data.shape[0]).tolist()
 
 print(graph.es[0].attributes())
-supervised_random_walks(graph, [graph.vs[0].index], [graph.vs[1, 2].indices])
-
+start = time.time()
+w = supervised_random_walks(graph, [graph.vs[0].index], [graph.vs[1, 3].indices])
+print(w)
+print(time.time() - start)
